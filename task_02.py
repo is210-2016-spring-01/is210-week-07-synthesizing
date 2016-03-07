10f198dac7f91f595efa290ff0078b249c258ecb
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-07 02:52:27 +0000

    Week 7 Warmup

---
+++ task_02.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Authentication module."""
+
+import authentication
+import getpass
+
+
+def login(username, maxattempts=3):
+    """Login module.
+
+    Args:
+        username (str): Name of user.
+        maxattempts (int, optional): Max number of attempts, defaults to 3.
+
+    Returns:
+        bool: Whether login is successful or not (True/False).
+
+    Examples:
+        >>>xxx
+        xxx
+
+        >>>xxx
+        xxx
+    """
+    auth = False
+    while 
