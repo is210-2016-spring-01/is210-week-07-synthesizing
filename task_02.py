f3147ce8e47eedba0ffaeaf7813233c4b6a60129
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-13 20:44:44 +0000

    Week 7 synth

--- task_02.py
+++ task_02.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """Authentication module."""
 
-import authentication
 import getpass
+import authentication
 
 
 def login(username, maxattempts=3):
@@ -17,11 +17,23 @@ def login(username, maxattempts=3):
         bool: Whether login is successful or not (True/False).
 
     Examples:
-        >>>xxx
-        xxx
+        >>> task_02.login('violet', 2)
+        Please enter your password:
+        Incorrect username or password. You have 1 attempts left.
+        Please enter your password:
+        Incorrect username or password. You have 0 attempts left.
+        False
 
-        >>>xxx
-        xxx
+        >>> task_02.login('mike', )
+        Please enter your password:
+        True
     """
     auth = False
-    while 
+    wrong = 'Incorrect username or password. You have {0} attempts left.'
+    while auth is False and maxattempts > 0:
+        password = getpass.getpass('Please enter your password: ')
+        auth = authentication.authenticate(username, password)
+        if auth is False:
+            maxattempts -= 1
+            print wrong.format(maxattempts)
+    return auth
